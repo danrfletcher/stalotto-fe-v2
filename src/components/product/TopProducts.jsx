c45625a55f6ec9254e50c1daa9d6e1d0184b4e34
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-20 15:16:57 +0000

    Add business logic to open various competition categories on page load

--- src/components/product/TopProducts.jsx
+++ src/components/product/TopProducts.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
 import useActive from '../../hooks/useActive';
 import competitionsData from '../../data/competitionData';
 import ProductCard from './ProductCard';
+import commonContext from '../../contexts/common/commonContext';
 
 
 const TopProducts = () => {
 
+    const { setCurrentHash, currentHash } = useContext(commonContext);
     const [products, setProducts] = useState(competitionsData);
     const { activeClass, handleActive } = useActive(0);
 
@@ -30,6 +32,35 @@ const TopProducts = () => {
         handleActive(i);
     };
 
+    // handle smooth scrolling to elements on the page
+    useEffect(() => {
+        console.log("currentHash:", currentHash.slice(1));
+        if (currentHash.startsWith('#comp-')) {
+            const hashCategory = currentHash.replace('#comp-', '').toLowerCase();
+            const encodedProductsCategory = productsCategory.map(category => category.toLowerCase().split(' ').join('-'));
+            
+            if (encodedProductsCategory.includes(hashCategory)) {
+                const categoryIndex = encodedProductsCategory.indexOf(hashCategory);
+                handleActive(categoryIndex); // Activate the relevant competition tab
+                const decodedCategory = productsCategory[categoryIndex]; 
+                setProducts(competitionsData.filter(item => item.state === decodedCategory)); // Set the correct products
+
+                //handleProducts(decodedCategory, categoryIndex);
+
+                document.getElementById('comp')?.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        } else if (currentHash.length === 0) {
+            document.body?.scrollIntoView({
+                behavior: 'smooth'
+              });
+        } else {
+            document.getElementById(currentHash.slice(1))?.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    }, [currentHash]);
 
     return (
         <>
