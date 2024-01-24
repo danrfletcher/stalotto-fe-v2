da119569cff2b213dc2378c7b8a1f856d70ffbe6
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-24 13:42:41 +0000

    Edit Vite config to use ssl environment variables for https in development environment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  server: {
-    host: true
-  }
-})
+console.log(process.env.VITE_MKCERT_KEY)
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: 'stalotto.test',
+      https: {
+        key: process.env.VITE_MKCERT_KEY,
+        cert: process.env.VITE_MKCERT_CERT
+      },
+    },
+  })
+};
