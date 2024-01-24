de8410243f156622a4eaaac2a06f4b2e72a90ac5
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-24 13:47:25 +0000

    Add business logic to render custom product offers

---
+++ src/pages/OffersList.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { offersData } from "../data/offersData"
+
+interface ProductOffer {
+    category: string,
+    offerId: number,
+    parameters: (string | number)[]
+}
+
+export const OffersList = ({ offers }) => {
+    const [offersList, setOffersList] = useState<string[]>([]);
+    const interpolateOffers = (offersToInterpolate: ProductOffer[]): string[] => {
+        return offersToInterpolate.map((offerToInterpolate) => {
+            // Get offer category from data
+            const offerCategory = offersData.filter(currOfferFromData => currOfferFromData.category === offerToInterpolate.category)[0];
+            const offer = offerCategory.offers.filter(offer => offer.offerId === offerToInterpolate.offerId)[0];
+            let offerText = offer.title;
+    
+            // Interpolate each offer based on parameters
+            offer.parameters.forEach((parameter, i) => {
+                offerText = offerText.replace(`$${parameter.parameterId}`, String(offerToInterpolate.parameters[i]));
+            });
+    
+            return offerText;
+        });
+    };
+    
+    useEffect(() => {
+        setOffersList(interpolateOffers(offers));
+    }, [offers]);
+    
+
+    return (
+        <>
+            {offersList.map((offer, i) => (
+                <li key={i}>{offer}</li>
+            ))}
+        </>
+    )
+}
