61576852778e04a41e0e1e92ddb7e9471b01c83d
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-24 13:47:10 +0000

    Add new demo users for comments section data

--- src/data/usersData.js
+++ src/data/usersData.js
@@ -1,26 +1,45 @@
 const usersData = [
     {
         id: 1,
+        type: "creator",
+        username: "scottyfairno",
+        profileImg: "/images/users/scottyfairno-p.jpeg",
         tag: "featured-creator",
         tagline: "Win veg oil burning vehicles & more",
         heroImage: "/images/users/scottyfairno.png",
         heroVideo: "/videos/users/scottyfairno.mp4",
-        title: "scottyfairno",
-        connectivity: "Wireless",
         finalPrice: 1.90,
         originalPrice: 10,
-        path: "/u/scottyfairno",
     },
     {
         id: 2,
+        type: "creator",
+        username: "conallsraiments",
+        profileImg: "/images/users/connal-p.jpeg",
         tag: "featured-creator",
         tagline: "Win exclusive Raiments products & more",
         heroImage: "/images/users/connalsraiments.png",
         heroVideo: "/videos/users/connalsraiments.mp4",
-        title: "conallsraiments",
         finalPrice: 0.5,
         originalPrice: 5,
-        path: "/u/connalsraiments",
+    },
+    {
+        id: 3,
+        type: "user",
+        username: "uncle_buzzcocks",
+        profileImg: "/images/users/uncle_buzzcocks.jpeg"
+    },
+    {
+        id: 4,
+        type: "administrator",
+        username: "danrfletcher",
+        profileImg: "/images/users/danrfletcher.jpg"
+    },
+    {
+        id: 5,
+        type: "user",
+        username: "mrbodge",
+        profileImg: "/images/users/mrbodge.jpeg"
     },
 ]
 
