dd5702105dd3b97c44b055f17faaa6ae2922c0e1
Dan <danrfletcher@outlook.com>
2024-04-12 14:36:19 +0100

    Add silent fetching anonymous cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,43 @@ import Footer from './components/common/Footer';
 import BackTop from './components/common/BackTop';
 import loadingContext from './contexts/loading/loadingContext.jsx';
 import { useContext } from 'react';
+import { useEffect } from 'react';
+import useUserAccounts from './hooks/useUserAccounts.js';
+import useCartSync from './hooks/useCartSync.ts';
+import cartContext from './contexts/cart/cartContext.jsx';
 
 
 const App = () => {
-  const { isFirstLoad } = useContext(loadingContext);
+  const { isFirstLoad, isUserDataLoaded, toggleItemsLoaded } = useContext(loadingContext);
+
+  const { handleUserLogin } = useUserAccounts();
+  const { updateCartFromId } = useCartSync();
+
+  //Check for user token, silently log in user & update their cart
+  const restoreState = async () => {
+
+    const customerCredentials = {
+      savedToken: localStorage.userToken ? localStorage.userToken : false,
+      savedCartId: localStorage.cartId ? localStorage.cartId : false,
+    };
+    const { savedToken, savedCartId } = customerCredentials;
+
+    try {
+      if (savedToken) {
+        const doSilentLogin = await handleUserLogin(localStorage.userToken); //Log user in using token
+      } else if (savedCartId) {
+        const retrieveAnonymousCart = await updateCartFromId(savedCartId);
+      }
+      toggleItemsLoaded(['isUserDataLoaded']);
+
+    } catch (err) {
+
+    };
+  };
+
+  useEffect(()=> {
+    restoreState()
+  },[])
 
   return (
     <div className="app_container">
