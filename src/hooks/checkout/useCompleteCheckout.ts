1ca020a617dfe1e4b95131b8e38278d1169a75be
Dan <danrfletcher@outlook.com>
2024-04-28 20:55:07 +0100

    Adds order creation on back-end

--- src/hooks/checkout/useCompleteCheckout.ts
+++ src/hooks/checkout/useCompleteCheckout.ts
@@ -79,11 +79,16 @@ const useCompleteCheckout = () => {
     useEffect(() => {
         if (state.matches('createPayment')) {
             send({ type: 'CREATE_PAYMENT_REQUEST_SENT' });
-            handleCreateBoodilPayment({ uuid, consentToken });
+
+            const { cartId } = localStorage;
+            handleCreateBoodilPayment({ uuid, consentToken, cartId });
         }
         if (state.matches('createOrder')) {
-            send({ type: 'CREATE_ORDER_REQUEST_SENT' });
-            _handleCreateBoodilOrder({ uuid }, '200');
+            send({ type: 'CREATE_ORDER_ANIMATION_STARTED' });
+            localStorage.removeItem('cartId');
+            setTimeout(() => {
+                send({ type: 'ORDER_CREATED' });
+            }, 5000);
         }
     }, [state.value]);
     //----------------------------------------------------------------------------------------------------
--- src/machines/checkoutCompletionMachine.ts
+++ src/machines/checkoutCompletionMachine.ts
@@ -13,7 +13,7 @@ import { CreateBoodilPaymentResponse } from '../__generated__/graphql';
  */
 export const checkoutCompletionMachine = createMachine(
     {
-        /** @xstate-layout N4IgpgJg5mDOIC5QGMAWZkGsD2BXALgMLYC2ADgDZj4CW2AdgHRoaYAqATgIb2xfK0GhDpDD1aXCrADEARQCqAUQBKATQD6ABQCCy7QFkAyuoBiAeXkA5ACIBtAAwBdRKDLZYNQfRcgAHogBaAHYAJgAWRgBmADYQoOiwoKTIgEYQ6IAaEABPRDj7RjjIsIBOEsiYlOiSgFYAXzqsliw8IlJKajomZvZuXn4vYVFxGkkZSzN1BRUNHT0jB2ckEDcPLx9-BACU+xSgxmj7Ypqa+yCakqOs3IQgqsYLxLLElMiADhKghqb0FoJichULzMERcWj0KCaLjZEhifDSNgASX0igsbEWPlWni6G0C4TejBK0TeQWKQTCaR2JTC10QMRqjFKZWqYTe4RCKRK3xAPVaAI6wOQoPBkOhsPECORqPk6JSS1c7mxDFxWxCIRKhPiNTC0WJFTuKVpCFKBTCNUi9mikSJrLCkXO3N5-3aQK6ILAYJoEKhMLh0kIykU2jYii02lUKMsbFM2kRABlFHYnJjFetlpswgVrW8dUc3m8Um9Io8jYcUozzZbrQkc+9Hb8cM7AZ0GO7Pd6xX6dBHFFH1AGgyGk-KVqmcem8iFIoUwk97DUgrt7NSaTk8uVGPYt1v4urrWbIvXWHyXS3usKwGYOBAwBx-YHg6GzMprCp1IHpoZo4Ze+jk8ssTTUBNmiNJGCCAsQhqI0903bczlicpqXNI8-jaZtBWFL0oCvG87yRFE0QxACx2VCcEFifY3guGIdVeGodkyNcEHpB5txOQtzXJLlGh5BsTwwt0hQ9EVcNvSVCJlWw5RTNZx2AxAEgKZDimo+wp3JVcblOcsDyrG1awdXinXQgUhKwiExLvZ9X2UfsHyHYiFTksiFIQFIdgJKcwhCItZ1CaIaiYm5YPghC9xU1DG1M11W2E9scOvcSB0fdQbLfExYwTYdZKVbxyJ2e1wKZFk2R8zkjXeLz0guXzEiLbVD2M-imzMuKLx9cV4RSkMwx7PsPyUL91B-KMnNHFz8rcgJ1QiUk7ROHNqTSGCNzCgtqTeS03gaXj6GwG94GWEz+ViqaJrylVtnoh4jULEJwNJU4QnsAtQnSHbmuPVqzuYBtOB4PgBC6IYbxGMZcqAvxAk8-ZoOY0L4N3JCDyigS2vPESwAB-pgYYRQOA4bAOEh+TofcqCHo5dIzULHMSSNUICSCT4qS216SkLNGfrPNsRU6uFSdc8n1QOZd7UzQ5xZzVbpzC5H9xQr60NO3n4vwS8kpJkjJpVWdogeCC0lOMLLVluDtwVyLlei1XMJE7CrKF87NnKApzWN835cQxWmp+b6YrVjrO3EZ2rrNAo2XUjayo5EovaRn3rf9lXT3tsEwAF8QCaJ7XnMugqPoeXU9x8w4p0iUtEkKOJySChdrSSbnA-TjWrJz4mw8LklCmiO4PlnI51Xh7TYk1ZItt8rdIhCZu7bdYm8LtsAu7c15l0ZQtdUKu07mC9dy3OafrXKRd1N2uogA */
+        /** @xstate-layout N4IgpgJg5mDOIC5QGMAWZkGsD2BXALgMLYC2ADgDZj4CW2AdgHRoaYAqATgIb2xfK0GhDpDD1aXCrADEARQCqAUQBKATQD6ABQCCy7QFkAyuoBiAeXkA5ACIBtAAwBdRKDLZYNQfRcgAHogBaAHYAJgAWRgBmADYQoOiwoKTIgEYQ6IAaEABPRDj7RjjIsIBOEsiYlOiSgFYAXzqsliw8IlJKajomZvZuXn4vYVFxGkkZSzN1BRUNHT0jB2ckEDcPLx9-BACU+xSgxmj7Ypqa+yCakqOs3IQgqsYLxLLElMiADhKghqb0FoJichULzMERcWj0KCaLjZEhifDSNgASX0igsbEWPlWni6G0C4TejBK0TeQWKQTCaR2JTC10QMRqjFKZWqYTe4RCKRK3xAPVaAI6wOQoPBkOhsPECORqPk6JSS1c7mxDFxWxCIRKhPiNTC0WJFTuKVpCFKBTCNUi9mikSJrLCkXO3N5-3aQK6ILAYJoEKhMLh0kIykU2jYii02lUKMsbFM2kRABlFHYnJjFetlpswgVrW8dUc3m8Um9Io8jYcUozzZbrQkc+9Hb8cM7AZ0GO7Pd6xX6dBHFFH1AGgyGk-KVqmcem8iFIoUwk97DUgrt7NSaTk8uVGPYt1v4urrWbIvXWHyXS3usKwGYOBAwBx-YHg6GzMprCp1NpLMjg4izJZ1IY2F0IcMWWLE01ATZojSRgggLEIaiNPdN23M5YnKalzSPP42mbQVhS9KArxvO9n1fZR+wfYDk1AsdlQnBAUkYiJbRCItZ1CaIakyNcECQlDUL3DDD0aHkGxPXC3SFD18DAH1xXhAdHzDHs+0DaYAP-Xt0WohU1nHCC8WpGDimLGoc2pNJEI3fiC2pN5LTeBoRPobAb3gZYnRwgV9NHPS6IMrZGOLB4jULEIYNJGo1XsN4aiqOsRM8-lXVbHpOB4PgBC6IYbxGMYUz87x6O2GL9gQni+JQ3d0IPLDGy8lLz2ksB0v6LKGEUDgOGwDgCqVIqArSKLCjSBI4vzVkgiNUICSCT4qXsmKSkLOrxO81L8I7X1xD68C-HXA5l3tTNDiOnMrOnfjqv3TDErEpt1qasFL2vW9dp8jMdQeWChv4rduJuSrt2uoTVoexq2xFIi3powqVSJS7Ungi7kOBtCbuEn5j3Bs9Idkzsdth-qVQCM0CjZexWM5Nkwg5EoUau9HQbu7GGtxqTnrkuFOu63qib2zZRoJLj0dpw4p0iUtEkKOJyS4hdrSSMG2eBHriOS6gwHe-z9oYi0NQpN5dR2Ey7gB9dy3OLcKjKe0txCJy6iAA */
         id: 'checkoutCompletion',
         initial: 'checkTransactionCredentials',
         context: {
@@ -47,9 +47,11 @@ export const checkoutCompletionMachine = createMachine(
                     NO_QUERY_PARAMS: 'createTransactionError',
                 },
             },
+
             createTransactionError: {
                 type: 'final',
             },
+
             creatingPayment: {
                 on: {
                     TIMEOUT: [
@@ -64,39 +66,32 @@ export const checkoutCompletionMachine = createMachine(
                     PAYMENT_CREATED: 'createOrder'
                 },
             },
+
             createOrder: {
                 on: {
-                    CREATE_ORDER_REQUEST_SENT: 'creatingOrder',
+                    CREATE_ORDER_ANIMATION_STARTED: 'creatingOrder'
                 },
             },
+
             creatingOrder: {
                 on: {
-                    TIMEOUT: [
-                        {
-                            target: 'createOrder',
-                            actions: 'incrementOrderRetries',
-                            guard: 'canRetryOrder',
-                        },
-                        'createOrderError',
-                    ],
-                    CREATE_ORDER_FAILED: 'createOrderError',
-                    ORDER_CREATED: 'orderComplete',
+                    ORDER_CREATED: 'orderComplete'
                 },
             },
+
             createPayment: {
                 on: {
                     CREATE_PAYMENT_REQUEST_SENT: 'creatingPayment',
                 },
             },
+
             createPaymentError: {
                 type: 'final',
             },
-            createOrderError: {
-                type: 'final',
-            },
+
             orderComplete: {
                 type: 'final',
-            },
+            }
         },
     },
     //----------------------------------------------------------------------------------------------------
